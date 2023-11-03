54a50f91d94b67839eaca51d6405958d1c518f91
Nguyen Huu Huy <huuhuy.nguyen1991@gmail.com>
2023-11-03 18:52:01 +0700

    add

---
+++ Example/Tests/SwiftReflectionTests.swift
@@ -0,0 +1,12 @@
+//
+//  TestSwiftReflection.swift
+//  BaseCoreData_Tests
+//
+//  Created by Nguyen Huu Huy on 03/11/2023.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import CoreData
+import BaseCoreData
